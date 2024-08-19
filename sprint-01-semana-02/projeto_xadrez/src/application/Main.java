c51495d5ad52032612b9885121a67c73c5d1e16a
Gustavo Bonfim <dev.gusta.io@gmail.com>
2024-08-19 16:16:52 -0300

    Second week - Checkmate logic

--- sprint-01-semana-02/projeto_xadrez/src/application/Main.java
+++ sprint-01-semana-02/projeto_xadrez/src/application/Main.java
@@ -16,7 +16,7 @@ public class Main {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
-        while (true){
+        while (!chessMatch.getCheckMate()){
             try {
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
@@ -45,7 +45,10 @@ public class Main {
                 System.out.println(c.getMessage());
                 sc.nextLine();
             }
-
         }
+
+        UI.clearScreen();
+
+        UI.printMatch(chessMatch, captured);
     }
 }
