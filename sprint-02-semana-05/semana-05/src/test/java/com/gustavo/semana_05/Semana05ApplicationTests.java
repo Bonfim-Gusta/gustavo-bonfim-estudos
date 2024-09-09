e0acae81f7e21bb18d5c486a841f798b2b0ec824
Gustavo Bonfim <dev.gusta.io@gmail.com>
2024-09-09 13:36:26 -0300

    Projeto semana 05

---
+++ sprint-02-semana-05/semana-05/src/test/java/com/gustavo/semana_05/Semana05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.semana_05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semana05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
